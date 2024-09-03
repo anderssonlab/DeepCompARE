bcc43a8bbb9194836aaef71bc0d0c317f35dfcc0
pcr980 <pcr980@scarbrna01fl.unicph.domain>
2024-09-03 14:33:23 +0200

    update expressed TFs

--- Curate_motif_annot/README.txt
+++
@@ -1,9 +0,0 @@
-This directory subset Jaspar database by transcription factors expressed in HegP2 and K562 cells.
-Then add ChIP-Seq info to distinguish putative binding from actual binding.
-
-Raw_data/:
-Total RNA-Seq of HepG2 is downloaded from https://www.encodeproject.org/experiments/ENCSR181ZGR/
-Total RNA-Seq of K562 is downloaded from https://www.encodeproject.org/experiments/ENCSR792OIJ/
-
-curate.R:
-Input gene expression data, output lists of expressed and unexpressed TF, for HepG2 and K562 cell lines.
--- RNA_expression/curate.R
+++ RNA_expression/curate.R
@@ -1,4 +1,4 @@
-setwd("/maps/projects/ralab/people/pcr980/DeepCompare/Curate_motif_annot/")
+setwd("/maps/projects/ralab/people/pcr980/DeepCompare/RNA_expression/")
 
 library(rtracklayer)
 library(biomaRt)
@@ -13,7 +13,7 @@ expr_k562$ensembl <- ifelse(grepl("^ENSG", expr_k562$gene_id), sub("\\.[0-9]+$",
 
 
 # read in TF-gene name mapper
-name_mapper <- read.csv("/maps/projects/ralab/people/pcr980/Resource_HTF/DatabaseExtract_v_1.01.csv",row.names = 1)
+name_mapper <- read.csv("/maps/projects/ralab/people/pcr980/Resource/HTF/DatabaseExtract_v_1.01.csv",row.names = 1)
 sum(name_mapper$Is.TF.=="Yes")
 name_mapper <- name_mapper %>% filter(grepl("^ENSG",Ensembl.ID))
 rownames(name_mapper) <- name_mapper$Ensembl.ID
@@ -49,8 +49,8 @@ write.csv(tf_expr_k562,"TF_expression_k562.csv")
 #-------------------------------------------------------
 
 # remap: TF with ENCODE ChIP experiments in HepG2 and K562
-remap_hepg2 <- import.bed("/maps/projects/ralab/people/pcr980/Resource_ReMap2022/ReMap2022_Hep-G2.bed")
-remap_k562 <- import.bed("/maps/projects/ralab/people/pcr980/Resource_ReMap2022/ReMap2022_K-562.bed")
+remap_hepg2 <- import.bed("/maps/projects/ralab/people/pcr980/Resource/ReMap2022/ReMap2022_Hep-G2.bed")
+remap_k562 <- import.bed("/maps/projects/ralab/people/pcr980/Resource/ReMap2022/ReMap2022_K-562.bed")
 
 tf_hepg2 <-  sub(":.*", "", remap_hepg2$name)
 tf_hepg2 <- unique(tf_hepg2)
@@ -65,10 +65,10 @@ tf_expr_k562_sub <- tf_expr_k562 %>% filter(HGNC.symbol %in% tf_k562) # two drop
 #-------------------------------------------------------
 # Create a list of expressed and nonexpressed TFs for hepG2 and K562
 #-------------------------------------------------------
-tf_expressed_hepg2 <- tf_expr_hepg2 %>% filter(TPM>0) %>% pull(HGNC.symbol) 
+tf_expressed_hepg2 <- tf_expr_hepg2 %>% filter(TPM>1) %>% pull(HGNC.symbol) 
 tf_expressed_hepg2 <- unique(c(tf_expressed_hepg2,tf_hepg2))
 
-tf_expressed_k562 <- tf_expr_k562 %>% filter(TPM>0) %>% pull(HGNC.symbol) 
+tf_expressed_k562 <- tf_expr_k562 %>% filter(TPM>1) %>% pull(HGNC.symbol) 
 tf_expressed_k562 <- unique(c(tf_expressed_k562,tf_k562))
 
 
