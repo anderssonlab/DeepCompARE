b2f7555e9d2f9a40a6e981a5a07e66435dde0102
Xuening He <54844911+XueningHe@users.noreply.github.com>
2025-06-18 15:25:29 +0200

    add_starr_loose_preprocessing

---
+++ Filter_regions/Rcode/STARR_loose_preprocessing.R
@@ -0,0 +1,14 @@
+setwd("/maps/projects/ralab/people/pcr980/DeepCompare/Filter_regions/")
+source("/maps/projects/ralab/people/pcr980/DeepCompare/Scripts_R/Generate_data/Function_utils.R")
+
+
+data_dir <- "STARR/STARRPeaker_res/"
+
+gr <- read_file(paste0(data_dir,"STARR_HepG2.peak.final.starrPeak"))
+gr <- gr[gr$pval>-log10(0.2)] 
+export.bed(gr,"/maps/projects/ralab/people/pcr980/DeepCompare/Pd1_bed_processed/STARR_HepG2_loose.bed")
+rm(gr)
+
+gr <- read_file(paste0(data_dir,"STARR_K562.peak.final.starrPeak"))
+gr <- gr[gr$pval>-log10(0.2)] 
+export.bed(gr,"/maps/projects/ralab/people/pcr980/DeepCompare/Pd1_bed_processed/STARR_K562_loose.bed")
